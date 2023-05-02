75baaaaa5deb37f6c32b2f09cd3a9fbf8a515720
Indra_Bagus_GolB_mobile <indrabusiness00@gmail.com>
2023-05-02 22:25:34 +0700

    sweetalert2

--- resources/views/pages/produk/produk.blade.php
+++ resources/views/pages/produk/produk.blade.php
@@ -165,41 +165,23 @@
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
     <script type="text/javascript">
         $(function() {
-                $(document).on('click', '# ', function(e) {
+                $(document).on('click', '#hapus ', function(e) {
                     e.preventDefault();
                     var link = $(this).attr("href");
-
-                    const swalWithBootstrapButtons = Swal.mixin({
-                        customClass: {
-                            confirmButton: 'btn btn-success',
-                            cancelButton: 'btn btn-danger'
-                        },
-                        buttonsStyling: false
-                    })
-
-                    swalWithBootstrapButtons.fire({
+                    Swal.fire({
                         title: 'Are you sure?',
                         text: "You won't be able to revert this!",
                         icon: 'warning',
                         showCancelButton: true,
-                        confirmButtonText: 'Yes, delete it!',
-                        cancelButtonText: 'No, cancel!',
-                        reverseButtons: true
+                        confirmButtonColor: '#3085d6',
+                        cancelButtonColor: '#d33',
+                        confirmButtonText: 'Yes, delete it!'
                     }).then((result) => {
                         if (result.isConfirmed) {
-                            swalWithBootstrapButtons.fire(
+                            Swal.fire(
                                 'Deleted!',
                                 'Your file has been deleted.',
                                 'success'
-                            )
-                        } else if (
-                            /* Read more about handling dismissals below */
-                            result.dismiss === Swal.DismissReason.cancel
-                        ) {
-                            swalWithBootstrapButtons.fire(
-                                'Cancelled',
-                                'Your imaginary file is safe :)',
-                                'error'
                             )
                         }
                     })
